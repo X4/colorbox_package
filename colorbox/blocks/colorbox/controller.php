deec699d0273d100433c373f972b89258ff4b650
Fernandos <worldwide@online.de>
2010-03-10 22:48:23 +0100

    - removed trailing spaces - bot test

--- colorbox/blocks/colorbox/controller.php
+++ colorbox/blocks/colorbox/controller.php
@@ -1,6 +1,6 @@
-<?php  
+<?php
 	defined('C5_EXECUTE') or die(_("Access Denied."));
-	
+
 	class ColorBoxBlockController extends BlockController {
 
 		protected $btName = "ColorBox";
@@ -8,13 +8,13 @@
 		protected $btInterfaceWidth = 500;
 		protected $btInterfaceHeight = 350;
 		protected $btTable = 'btColorBox';
-		
+
 		public function getBlockTypeDescription() {
 			return t("A versatile lightbox addon for Concrete5.");
 		}
-		
+
 		public function getBlockTypeName() {
 			return t("ColorBox");
-		}	
+		}
 	}
 ?>
--- colorbox/controller.php
+++ colorbox/controller.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
@@ -7,21 +7,21 @@ class ColorboxPackage extends Package {
 	protected $pkgHandle = 'colorbox';
 	protected $appVersionRequired = '5.2.0';
 	protected $pkgVersion = '1.0';
-	
+
 	public function getPackageDescription() {
 		return t("Add a colorbox to your site.");
 	}
-	
+
 	public function getPackageName() {
 		return t("Colorbox");
 	}
-	
+
 	public function install() {
 		$pkg = parent::install();
-		
-		// install block		
+
+		// install block
 		BlockType::installBlockTypeFromPackage('colorbox', $pkg);
-		
+
 	}
 
 
